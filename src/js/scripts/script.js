250ab53434d6856b346893c658f22e0a1a3d337e
Kirill Demidov <kademidov@gmail.com>
2017-05-16 14:10:19 +0300

    правки + js для хедера и навигации

--- src/js/scripts/script.js
+++ src/js/scripts/script.js
@@ -6,4 +6,58 @@ $(document).ready(function(){
     dots: true
   });
 
+
+
+  // Плавный скролл по якорям
+  function anchorScroll(boxAnchorLink){
+      $(boxAnchorLink + ' a').on('click', function(e){
+          e.preventDefault();
+          var attr = $(this).attr('href').substring(1);
+          var currentPosition = $(document).scrollTop();
+          var idPosition = $('#'+attr).offset().top;
+          var scrollTime = Math.abs(currentPosition - idPosition) / 4; // Math.abs - модуль числа.
+          $('body,html').animate({'scrollTop':idPosition-70},scrollTime);
+      });
+  };
+
+  anchorScroll('.nav');
+
+
+  // бургер, что тут еще скажешь? но без колы(
+  $('.burger').on('click', function(e){
+      e.preventDefault();
+      if ( $(this).hasClass("burger--close") ) {
+          $(this).removeClass('burger--close');
+          $('.nav').slideUp(400);
+      } else {
+          $(this).addClass('burger--close');
+          $('.nav').slideDown(400);
+      }
+  });
+
+  $('.nav ul a').on('click', function(e){
+      if ($(window).width() < 1200) {
+          $('.nav').slideUp(400);
+          $('.burger').addClass('burger--open');
+          $('.burger').removeClass('burger--close');
+      };
+  });
+
+
+  $(window).resize(function(){
+      if( $(window).width() > 1199 ) {
+          $('.nav').show();
+      } else {
+          $('.nav').slideUp(400);
+          $('.burger').removeClass('burger--close');
+      }
+  });
+
+  $(window).on('scroll', function(e) { // отслеживаем событие на элементе window
+    if( $(window).width() < 1200 ) {
+        $('.nav').slideUp(400);
+        $('.burger').removeClass('burger--close');
+    }
+  });
+
 });
